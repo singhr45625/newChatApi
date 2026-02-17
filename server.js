3e08c7da5e6791168888b306b6c31cb659a5ec54
rishu2004-rs <kpuja0969@gmail.com>
2026-02-17 18:01:17 +0530

    Fix Render deployment: Add health check and update CORS

--- server.js
+++ server.js
@@ -20,21 +20,25 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "https://newchatapi.onrender.com"],
         credentials: true,
     })
 );
 
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//     });
+// }
 
 server.listen(PORT, () => {
     console.log("Server is running on PORT: " + PORT);
